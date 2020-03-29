b5b5b5bce164196c16b6beb847b59eea00930d9d
CJ Kenney <cjkenney.dev@gmail.com>
2020-03-29 16:03:35 -0500

    add handleKeyPress for enter key

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -5,14 +5,28 @@ export default () => {
   const [todos, setTodos] = useState(initialTodos);
   const [inputTodo, setInputTodo] = useState('');
 
+  const handleNewTodo = () => {
+    if (inputTodo !== '') {
+      setTodos([...todos, inputTodo]);
+      setInputTodo('');
+    }
+  };
+
+  const handleKeyPress = event => {
+    if (event.keyCode === 13) {
+      handleNewTodo();
+    }
+  };
+
   return (
     <>
       <input
         value={inputTodo}
         onChange={v => setInputTodo(v.target.value)}
+        onKeyDown={handleKeyPress}
         placeholder='Enter A Task'
       ></input>
-      <button onClick={() => setTodos([...todos, inputTodo])}>Add Task</button>
+      <button onClick={handleNewTodo}>Add Task</button>
       <ul>
         {todos.map(todo => (
           <li>{todo}</li>
