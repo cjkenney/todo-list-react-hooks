a8ddc72263c3726a16bbcbc81a480bb820cf45c2
CJ Kenney <cjkenney.dev@gmail.com>
2020-03-29 13:44:18 -0500

    add "add todo" button

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,12 +3,21 @@ import { initialTodos } from '../constants/index';
 
 export default () => {
   const [todos, setTodos] = useState(initialTodos);
+  const [inputTodo, setInputTodo] = useState('');
 
   return (
-    <ul>
-      {todos.map(todo => (
-        <li>{todo}</li>
-      ))}
-    </ul>
+    <>
+      <input
+        value={inputTodo}
+        onChange={v => setInputTodo(v.target.value)}
+        placeholder='Enter A Task'
+      ></input>
+      <button onClick={() => setTodos([...todos, inputTodo])}>Add Task</button>
+      <ul>
+        {todos.map(todo => (
+          <li>{todo}</li>
+        ))}
+      </ul>
+    </>
   );
 };
